cc061d98b10da3623268e83d774432976599a032
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-09 20:48:52 +0100

    Add Getters&Setters to ExperienceLevel class.

--- src/main/java/com/codecool/idontspeakjava/queststore/models/ExperienceLevel.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/models/ExperienceLevel.java
@@ -1,6 +1,29 @@
+package com.codecool.idontspeakjava.queststore.models;
+
 public class ExperienceLevel{
-    
+
     private int id;
     private String name;
     private long threshold;
+
+    public ExperienceLevel(String name, long threshold) {
+        this.name = name;
+        this.threshold = threshold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(long threshold) {
+        this.threshold = threshold;
+    }
 }
