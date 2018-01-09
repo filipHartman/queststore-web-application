298b1d066611aa994aa3289b157b6ee76952cf01
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-09 20:57:12 +0100

    Add Setters&Getters and Constructor for Team class.

--- src/main/java/com/codecool/idontspeakjava/queststore/models/Team.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/models/Team.java
@@ -1,5 +1,19 @@
+package com.codecool.idontspeakjava.queststore.models;
+
 public class Team{
-    
+
     private int id;
     private String name;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
