af2b2b55b3f2e33bde3813365ac94782eb6fea8a
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-09 19:46:50 +0100

    Implement AbstractDAO class.

---
+++ src/main/java/com/codecool/idontspeakjava/queststore/database/AbstractDAO.java
@@ -0,0 +1,29 @@
+package com.codecool.idontspeakjava.queststore.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AbstractDAO implements Database {
+
+    private Connection connection = null;
+
+    @Override
+    public void connectToDatabase() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:quest_store.db?foreign_keys=on;");
+        connection.setAutoCommit(true);
+    }
+
+    @Override
+    public Connection getConnection() {
+        if (connection == null) {
+            try {
+                connectToDatabase();
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
