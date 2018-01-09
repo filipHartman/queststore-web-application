1bcfd37d825b3f497df024d0c771883ac0476547
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-09 20:54:54 +0100

    Add Setters&Getters and Constructor for Quest class.

--- src/main/java/com/codecool/idontspeakjava/queststore/models/Quest.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/models/Quest.java
@@ -1,12 +1,49 @@
+package com.codecool.idontspeakjava.queststore.models;
+
 public class Quest{
 
-    enum QuestCategory{
-        Basic, Extra;
-    }
-    
     private int id;
     private String title;
     private QuestCategory category;
     private String description;
     private int reward;
+
+    public Quest(String title, QuestCategory category, String description, int reward) {
+        this.title = title;
+        this.category = category;
+        this.description = description;
+        this.reward = reward;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public QuestCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(QuestCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
 }
