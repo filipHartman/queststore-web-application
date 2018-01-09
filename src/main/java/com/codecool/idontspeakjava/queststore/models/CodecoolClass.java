be0adaac8d09faa97dd91b2937e1a419d4704dfe
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-09 20:48:12 +0100

    Add Getters&Setters to CodecoolClass class.

--- src/main/java/com/codecool/idontspeakjava/queststore/models/CodecoolClass.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/models/CodecoolClass.java
@@ -1,5 +1,19 @@
+package com.codecool.idontspeakjava.queststore.models;
+
 public class CodecoolClass{
-    
+
     private int id;
     private String name;
+
+    public CodecoolClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
