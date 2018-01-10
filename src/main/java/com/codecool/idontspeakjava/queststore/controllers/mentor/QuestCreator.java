b53e4ca2ca2c5848056a112edd091f37ef22a3e2
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 13:56:38 +0100

    Create empty createQuest method. Add constant with EXIT value

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/QuestCreator.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/QuestCreator.java
@@ -1,4 +1,9 @@
 package com.codecool.idontspeakjava.queststore.controllers.mentor;
 
 public class QuestCreator {
+
+    private static final String EXIT = "0";
+
+    public void createQuest() {
+    }
 }
