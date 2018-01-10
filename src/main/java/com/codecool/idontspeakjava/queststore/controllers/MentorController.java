529b2fd489916c51d0b72ae9af6a490d7ec01ef5
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 11:20:40 +0100

    Invoke showWrongInput from the view after invalid input

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
@@ -63,6 +63,7 @@ public class MentorController {
                 continueRunning = false;
                 break;
             default:
+                view.showWrongInput();
         }
         return continueRunning;
     }
