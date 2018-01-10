b7e470f7496a64ae68864d4453a442f1e83e363b
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 10:46:15 +0100

    Add constants for options available for the mentor

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
@@ -8,6 +8,16 @@ public class MentorController {
     private MentorView view;
     private User mentor;
 
+    private static final String CREATE_CODECOOLER = "1";
+    private static final String ADD_QUEST = "2";
+    private static final String ADD_ARTIFACT = "3";
+    private static final String EDIT_QUEST = "4";
+    private static final String EDIT_ARTIFACT = "5";
+    private static final String MARK_QUEST = "6";
+    private static final String MARK_ARTIFACT = "7";
+    private static final String CHECK_WALLETS = "8";
+    private static final String EXIT = "0";
+
     public MentorController() {
         view = new MentorView();
         mentor = new User("Henryk", "Pryk", "ahbgd", "h.pryk@email.com", Permissions.Mentor);
