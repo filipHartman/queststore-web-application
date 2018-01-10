0620a32b8f736320edd006be59751425a11bfce4
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 20:50:06 +0100

    Add attributes, constants, imports and constructor

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/QuestCreator.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/QuestCreator.java
@@ -1,9 +1,25 @@
 package com.codecool.idontspeakjava.queststore.controllers.mentor;
 
-public class QuestCreator {
+import com.codecool.idontspeakjava.queststore.models.Quest;
+import com.codecool.idontspeakjava.queststore.models.QuestCategory;
+import com.codecool.idontspeakjava.queststore.views.MentorView;
+
+class QuestCreator {
+
+    private static final int TITLE = 0;
+    private static final int CATEGORY = 1;
+    private static final int DESCRIPTION = 2;
+    private static final int REWARD = 3;
 
     private static final String EXIT = "0";
+    private MentorView view;
 
-    public void createQuest() {
+    private String title;
+    private QuestCategory category;
+    private String description;
+    private int reward;
+
+    QuestCreator(MentorView view) {
+        this.view = view;
     }
 }
