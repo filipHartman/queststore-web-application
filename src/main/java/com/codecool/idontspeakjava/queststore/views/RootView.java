ba269db3469571f58d570ca0242b62042bb34381
adamlup <adamlupi@gmail.com>
2018-01-10 21:23:03 +0100

    add getUserInput method

--- src/main/java/com/codecool/idontspeakjava/queststore/views/RootView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/RootView.java
@@ -1,5 +1,13 @@
 package com.codecool.idontspeakjava.queststore.views;
 
+import java.util.Scanner;
+
 public class RootView extends UserView{
+
+    private Scanner scanner;
     
+    public String getUserInput(){
+        scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    } 
 }
