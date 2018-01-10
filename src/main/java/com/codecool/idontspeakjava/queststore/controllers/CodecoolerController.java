3250a427a8a2050a5ecb7835bd81dd6ae312dd67
przemekNachel <przemeknachel@gmail.com>
2018-01-10 12:52:11 +0100

    Implement idea of checkExperienceLevel

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/CodecoolerController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/CodecoolerController.java
@@ -3,28 +3,50 @@ package com.codecool.idontspeakjava.queststore.controllers;
 import com.codecool.idontspeakjava.queststore.views.CodecoolerView;
 import com.codecool.idontspeakjava.queststore.models.Team;
 import com.codecool.idontspeakjava.queststore.models.Artifact;
+import com.codecool.idontspeakjava.queststore.models.Wallet;
+import com.codecool.idontspeakjava.queststore.database.WalletDAO;
 import com.codecool.idontspeakjava.queststore.database.ArtifactDAO;
+import com.codecool.idontspeakjava.queststore.database.ExperienceLevelDAO;
 
 import java.util.List;
 
 public class CodecoolerController{
 
-    private CodecoolerView view = new CodecoolerView();
-    private ArtifactDAO artifactDAO;;
+    private CodecoolerView view;
+    private ArtifactDAO artifactDAO;
+    private WalletDAO walletDAO;
+    private ExperienceLevelDAO experienceLevelDAO;
+    private User user;
+    private Wallet wallet;
 
-    public CodecoolerController(){
+    public CodecoolerController(User user){
         this.artifactDAO = new ArtifactDAO();
+        this.experienceLevelDAO = new ExperienceLevelDAO();
+        this.walletDAO = new WalletDAO();
+        this.view = new CodecoolerView();
+        this.user = user;
+        this.wallet = walletDAO.getWalletByUserID(user.getID);
     }
 
     private boolean buyArtifact(){
+        Wallet userWallet = walletDAO.getWalletByUserID(user.getID);
         List<String> namesOfartifacts = new List<String>();
         for (Artifact artifact : artifactDAO.getAllArtifacts) {
-            namesOfartifacts.add(artifact.title);
+            namesOfartifacts.add(artifact.getTitle());
         }
     }
 
     private void checkExperienceLevel(){
-
+        long totalEarnings = wallet.getTotalEarnings();
+        List<ExperienceLevel> levels = experienceLevelDAO.getAllLevels();
+        ExpirienceLevel level = levels.get(0);
+        for (int i; i < levels.size(); i++) {
+            level = levels.get(i);
+            if (level.getThreshold() > totalEarnings) {
+                
+            }
+        }
+        view.showMyLevel(experienceLevelDAO.get);
     }
 
     private void checkWallet(){
