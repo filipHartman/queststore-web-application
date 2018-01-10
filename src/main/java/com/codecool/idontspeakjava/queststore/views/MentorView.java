50c3f3efe239d6e4bf4dbdd49d272514f5ed4ec4
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 10:31:20 +0100

    Create method which will print the main menu of the mentor

--- src/main/java/com/codecool/idontspeakjava/queststore/views/MentorView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/MentorView.java
@@ -1,5 +1,18 @@
 package com.codecool.idontspeakjava.queststore.views;
 
-public class MentorView {
 
+public class MentorView extends UserView {
+
+    public void showMainMenu() {
+        System.out.println("Select what you want to do:\n" +
+                "1 - Create new codecooler\n" +
+                "2 - Add new quest\n" +
+                "3 - Add new artifact\n" +
+                "4 - Edit existing quest\n" +
+                "5 - Edit existing artifact\n" +
+                "6 - Mark quest for a codecooler\n" +
+                "7 - Mark artifact for a codecooler\n" +
+                "8 - Check the wallets of codecoolers\n" +
+                "0 - \n");
+    }
 }
