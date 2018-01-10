dcf5b0d8ba93c934db361e206505a98705e82987
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 10:19:50 +0100

    Create constructor and instantiate in it the User instance for current user and MentorView instance

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/MentorController.java
@@ -1,5 +1,16 @@
 package com.codecool.idontspeakjava.queststore.controllers;
 
+import com.codecool.idontspeakjava.queststore.models.Permissions;
+import com.codecool.idontspeakjava.queststore.models.User;
+import com.codecool.idontspeakjava.queststore.views.MentorView;
+
 public class MentorController {
+    private MentorView view;
+    private User mentor;
+
+    public MentorController() {
+        view = new MentorView();
+        mentor = new User("Henryk", "Pryk", "ahbgd", "h.pryk@email.com", Permissions.Mentor);
+    }
 
 }
