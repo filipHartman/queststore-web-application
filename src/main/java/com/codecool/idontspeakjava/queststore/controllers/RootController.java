93b0638b3841794f23c67a1261b6fa11960ee8e8
adamlup <adamlupi@gmail.com>
2018-01-10 20:58:41 +0100

    create constructor, add view and user in it

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/RootController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/RootController.java
@@ -1,5 +1,14 @@
 package com.codecool.idontspeakjava.queststore.controllers;
 
-public class RootController{
+import com.codecool.idontspeakjava.queststore.views.RootView;
+import com.codecool.idontspeakjava.queststore.models.User;
 
+public class RootController{
+    private RootView view;
+    private User user;
+
+    public RootController(User user){
+        this.view = new RootView();
+        this.user = user;
+    }
 }
