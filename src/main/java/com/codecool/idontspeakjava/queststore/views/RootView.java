cdd88c49eda634756bed33b3836be78d07c4a7aa
adamlup <adamlupi@gmail.com>
2018-01-10 22:40:01 +0100

    add showCreateCodecoolClassMenu

--- src/main/java/com/codecool/idontspeakjava/queststore/views/RootView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/RootView.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class RootView extends UserView{
 
     private Scanner scanner;
-    
+
     public String getUserInput(){
         scanner = new Scanner(System.in);
         return scanner.nextLine();
-    } 
+    }
+    
+    public void showCreateCodecoolClassMenu(){
+        System.out.println("Enter class name");
+    }
 }
