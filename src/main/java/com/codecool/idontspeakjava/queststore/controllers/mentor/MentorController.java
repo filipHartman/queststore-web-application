fd99c8125c6c19f2b23654d8492a9e4264f9c107
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 13:49:04 +0100

    Move the code responsible for creation of codecooler from MentorController to CodecoolerCreator

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/MentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/MentorController.java
@@ -1,12 +1,8 @@
 package com.codecool.idontspeakjava.queststore.controllers.mentor;
 
-import com.codecool.idontspeakjava.queststore.database.UserDAO;
 import com.codecool.idontspeakjava.queststore.models.Permissions;
 import com.codecool.idontspeakjava.queststore.models.User;
 import com.codecool.idontspeakjava.queststore.views.MentorView;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class MentorController {
     private MentorView view;
@@ -22,9 +18,6 @@ public class MentorController {
     private static final String CHECK_WALLETS = "8";
     private static final String EXIT = "0";
 
-    private static final int STUDENT_NAME = 0;
-    private static final int STUDENT_SECOND_NAME = 1;
-    private static final int STUDENT_EMAIL = 2;
 
     public MentorController() {
         view = new MentorView();
@@ -44,7 +37,7 @@ public class MentorController {
         boolean continueRunning = true;
         switch (input) {
             case CREATE_CODECOOLER:
-                createCodecooler();
+                new CodecoolerCreator(view).createCodecooler();
                 break;
             case ADD_QUEST:
                 addQuest();
@@ -74,64 +67,6 @@ public class MentorController {
                 view.showWrongInput();
         }
         return continueRunning;
-    }
-
-    private void createCodecooler() {
-        int inputsReceived = 0;
-        boolean continueLoop = true;
-
-        final int PROMPTS = 3;
-
-        ArrayList<String> userInputs = new ArrayList<>();
-
-        for (int i = 0; i < PROMPTS && continueLoop; i++) {
-            selectPromptForCreateCodecooler(i);
-            String input = view.getUserInput();
-
-            if (input.equals(EXIT)) {
-                continueLoop = false;
-            } else {
-                userInputs.add(input);
-                inputsReceived++;
-            }
-        }
-
-        if (inputsReceived == PROMPTS) {
-            addCodecoolerToDatabase(userInputs);
-        } else {
-            view.showOperationCancelled();
-        }
-    }
-
-    private void selectPromptForCreateCodecooler(int promptNumber) {
-        switch (promptNumber) {
-            case STUDENT_NAME:
-                view.askForCodecoolerName();
-                break;
-            case STUDENT_SECOND_NAME:
-                view.askForSecondName();
-                break;
-            case STUDENT_EMAIL:
-                view.askForEmail();
-                break;
-        }
-    }
-
-    private void addCodecoolerToDatabase(ArrayList<String> userData) {
-        User newCodecooler = new User(
-                userData.get(STUDENT_NAME),
-                userData.get(STUDENT_SECOND_NAME),
-                "HASHTOPASS",
-                userData.get(STUDENT_EMAIL),
-                Permissions.Student);
-
-        try {
-            new UserDAO().createUser(newCodecooler);
-            view.showCodecoolerCreated();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            view.showCodecoolerCreationFailed();
-        }
     }
 
     private void addQuest() {
