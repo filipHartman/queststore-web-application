465b049016b4a97b906a03bf0ae8748c294f06f3
Jacek_R <jacek.ragann@gmail.com>
2018-01-10 10:51:52 +0100

    Add a userName parameter to the showMainMenu

--- src/main/java/com/codecool/idontspeakjava/queststore/views/MentorView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/MentorView.java
@@ -3,16 +3,17 @@ package com.codecool.idontspeakjava.queststore.views;
 
 public class MentorView extends UserView {
 
-    public void showMainMenu() {
-        System.out.println("Select what you want to do:\n" +
-                "1 - Create new codecooler\n" +
-                "2 - Add new quest\n" +
-                "3 - Add new artifact\n" +
-                "4 - Edit existing quest\n" +
-                "5 - Edit existing artifact\n" +
-                "6 - Mark quest for a codecooler\n" +
-                "7 - Mark artifact for a codecooler\n" +
-                "8 - Check the wallets of codecoolers\n" +
-                "0 - Exit the program\n");
+    public void showMainMenu(String userName) {
+        System.out.println(String.format(
+                "Hello %s ! Select what you want to do:\n" +
+                        "1 - Create new codecooler\n" +
+                        "2 - Add new quest\n" +
+                        "3 - Add new artifact\n" +
+                        "4 - Edit existing quest\n" +
+                        "5 - Edit existing artifact\n" +
+                        "6 - Mark quest for a codecooler\n" +
+                        "7 - Mark artifact for a codecooler\n" +
+                        "8 - Check the wallets of codecoolers\n" +
+                        "0 - Exit the program\n", userName));
     }
 }
