3456f8328cdba97776e0f3012a292d4981eac86d
przemekNachel <przemeknachel@gmail.com>
2018-01-10 10:34:12 +0100

    Add all methods from UML

--- src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
@@ -2,5 +2,19 @@ package com.codecool.idontspeakjava.queststore.views;
 
 public class CodecoolerView extends UserView{
 
+    public void showWallet(){
 
+    }
+
+    public void showBuyArtifactMenu(){
+
+    }
+
+    public void showBuyArtifactForTeamMenu(){
+
+    }
+
+    public void showMyLevel(){
+        
+    }
 }
