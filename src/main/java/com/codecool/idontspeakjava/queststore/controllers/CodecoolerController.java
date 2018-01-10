a124f9faccc703a4f77135810a13fd934f2b7524
przemekNachel <przemeknachel@gmail.com>
2018-01-10 11:31:58 +0100

    Implement buyArtifact

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/CodecoolerController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/CodecoolerController.java
@@ -2,13 +2,25 @@ package com.codecool.idontspeakjava.queststore.controllers;
 
 import com.codecool.idontspeakjava.queststore.views.CodecoolerView;
 import com.codecool.idontspeakjava.queststore.models.Team;
+import com.codecool.idontspeakjava.queststore.models.Artifact;
+import com.codecool.idontspeakjava.queststore.database.ArtifactDAO;
+
+import java.util.List;
 
 public class CodecoolerController{
 
     private CodecoolerView view = new CodecoolerView();
+    private ArtifactDAO artifactDAO;;
+
+    public CodecoolerController(){
+        this.artifactDAO = new ArtifactDAO();
+    }
 
     private boolean buyArtifact(){
-        return true;
+        List<String> namesOfartifacts = new List<String>();
+        for (Artifact artifact : artifactDAO.getAllArtifacts) {
+            namesOfartifacts.add(artifact.title);
+        }
     }
 
     private void checkExperienceLevel(){
