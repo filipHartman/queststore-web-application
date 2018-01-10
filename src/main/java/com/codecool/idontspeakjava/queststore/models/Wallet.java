383b004f0ec9798c19dba0f61f7057001661c8e0
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-10 11:56:45 +0100

    Add Builder pattern to Wallet class.

--- src/main/java/com/codecool/idontspeakjava/queststore/models/Wallet.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/models/Wallet.java
@@ -7,10 +7,18 @@ public class Wallet{
     private long currentState;
     private long totalEarnings;
 
-    public Wallet(int userID, long currentState, long totalEarnings) {
+    public Wallet(int id, int userID, long currentState, long totalEarnings) {
+        this.id = id;
         this.userID = userID;
         this.currentState = currentState;
         this.totalEarnings = totalEarnings;
+    }
+
+    private Wallet(Builder builder) {
+        setId(builder.id);
+        setUserID(builder.userID);
+        setCurrentState(builder.currentState);
+        setTotalEarnings(builder.totalEarnings);
     }
 
     public int getId() {
@@ -42,6 +50,42 @@ public class Wallet{
     }
 
     public void setTotalEarnings(long totalEarnings) {
+
         this.totalEarnings = totalEarnings;
+    }
+
+
+    public static final class Builder {
+        private int id;
+        private int userID;
+        private long currentState;
+        private long totalEarnings;
+
+        public Builder() {
+        }
+
+        public Builder setId(int val) {
+            id = val;
+            return this;
+        }
+
+        public Builder setUserID(int val) {
+            userID = val;
+            return this;
+        }
+
+        public Builder setCurrentState(long val) {
+            currentState = val;
+            return this;
+        }
+
+        public Builder setTotalEarnings(long val) {
+            totalEarnings = val;
+            return this;
+        }
+
+        public Wallet build() {
+            return new Wallet(this);
+        }
     }
 }
