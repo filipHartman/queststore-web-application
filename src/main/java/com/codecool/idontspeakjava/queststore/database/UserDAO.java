bca36a301e0063dcd56548d198d075ca972fb2a4
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-11 13:00:51 +0100

    Refactor getUserByEmail method.

--- src/main/java/com/codecool/idontspeakjava/queststore/database/UserDAO.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/database/UserDAO.java
@@ -34,12 +34,13 @@ public class UserDAO extends AbstractDAO {
     }
 
     public User getUserByEmail(String email) {
+        String query = String.format("SELECT * FROM users WHERE email ='%s'", email);
+        User user = null;
         try {
             if (checkIfUsersExists(email)) {
-                String query = String.format("SELECT * FROM users WHERE email ='%s'", email);
                 log.info(query);
                 ResultSet resultSet = getConnection().createStatement().executeQuery(query);
-                return new User.Builder()
+                user = new User.Builder()
                         .setId(resultSet.getInt("id"))
                         .setLastName(resultSet.getString("last_name"))
                         .setFirstName(resultSet.getString("first_name"))
@@ -52,7 +53,7 @@ public class UserDAO extends AbstractDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return user;
     }
 
     public List<User> getUsersByPermission(Permissions permission) {
