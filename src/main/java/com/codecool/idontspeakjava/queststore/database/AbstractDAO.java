e8f09a7c8e8d93520b25985892de1474acbb30d6
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-11 21:40:25 +0100

    Make class abstract to prevent instatiation.

--- src/main/java/com/codecool/idontspeakjava/queststore/database/AbstractDAO.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/database/AbstractDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class AbstractDAO implements Database {
+public abstract class AbstractDAO implements Database {
 
     private static Connection connection = null;
 
