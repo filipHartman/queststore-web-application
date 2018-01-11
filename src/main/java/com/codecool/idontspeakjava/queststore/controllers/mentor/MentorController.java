98140cf828d360cb555879921642512d955596f5
Jacek_R <jacek.ragann@gmail.com>
2018-01-11 15:09:55 +0100

    Remove redundant methods

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/MentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/MentorController.java
@@ -48,16 +48,12 @@ public class MentorController {
                 new QuestEditor(view).editQuest();
                 break;
             case EDIT_ARTIFACT:
-                editArtifact();
                 break;
             case MARK_QUEST:
-                markQuest();
                 break;
             case MARK_ARTIFACT:
-                markArtifact();
                 break;
             case CHECK_WALLETS:
-                checkWallets();
                 break;
             case EXIT:
                 continueRunning = false;
@@ -66,22 +62,5 @@ public class MentorController {
                 view.showWrongInput();
         }
         return continueRunning;
-    }
-
-
-    private void editArtifact() {
-
-    }
-
-    private void markQuest() {
-
-    }
-
-    private void markArtifact() {
-
-    }
-
-    private void checkWallets() {
-
     }
 }
