a5b7bfc341fd0696a27f90bcc4bafe0c88143780
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-11 13:02:36 +0100

    Implement aplication's main class.

--- src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
@@ -1,14 +1,11 @@
 package com.codecool.idontspeakjava.queststore.main;
 
-import com.codecool.idontspeakjava.queststore.database.AbstractDAO;
-
-import java.sql.SQLException;
+import com.codecool.idontspeakjava.queststore.controllers.LoginController;
 
 public class App {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        System.out.println("It works !!!");
-        new AbstractDAO().connectToDatabase();
-        new AbstractDAO().connectToDatabase();
+    public static void main(String[] args) {
+        new LoginController();
     }
+
 }
