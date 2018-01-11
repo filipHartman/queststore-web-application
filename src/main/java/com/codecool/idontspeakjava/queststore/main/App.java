f66e93ebdf857bf3a0c24d7668b301c86b0067c5
przemekNachel <przemeknachel@gmail.com>
2018-01-11 10:07:15 +0100

    Add menu positions in view

--- src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
@@ -4,6 +4,7 @@ import com.codecool.idontspeakjava.queststore.database.AbstractDAO;
 import com.codecool.idontspeakjava.queststore.database.UserDAO;
 import com.codecool.idontspeakjava.queststore.models.User;
 import com.codecool.idontspeakjava.queststore.models.Permissions;
+import com.codecool.idontspeakjava.queststore.controllers.codecooler.CodecoolerController;
 
 import java.sql.SQLException;
 
@@ -13,14 +14,7 @@ public class App {
         System.out.println("It works !!!");
         new AbstractDAO().connectToDatabase();       
 
-        UserDAO userDao = new UserDAO();
-        User user = new User("Przemek", "Nachel", "haslo", "cygan@nic.pl", Permissions.Student);
-        userDao.createUser(user);
-        user = userDao.getUserByEmail("cygan@nic.pl");
-        System.out.println(user.getFirstName());
-
-
-
-        
+        CodecoolerController controller = new CodecoolerController();
+        controller.run();
     }
 }
--- src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
@@ -7,14 +7,11 @@ public class CodecoolerView extends UserView{
     public void showMainMenu(String userName) {
     System.out.println(String.format(
             "Hello %s ! Select what you want to do:\n" +
-                    "1 - Create new codecooler\n" +
-                    "2 - Add new quest\n" +
-                    "3 - Add new artifact\n" +
-                    "4 - Edit existing quest\n" +
-                    "5 - Edit existing artifact\n" +
-                    "6 - Mark quest for a codecooler\n" +
-                    "7 - Mark artifact for a codecooler\n" +
-                    "8 - Check the wallets of codecoolers\n" +
+                    "1 - See your wallet\n" +
+                    "2 - Buy an artifact\n" +
+                    "3 - Buy an artifact for team\n" +
+                    "4 - See your level\n" +
+                    "5 - See available quests\n" +
                     "0 - Exit the program\n", userName));
     }
 
