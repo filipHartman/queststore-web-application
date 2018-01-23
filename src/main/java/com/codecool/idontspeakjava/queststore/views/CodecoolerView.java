f58f38c61a5459b8211b2cf33e246343e6144391
przemekNachel <przemeknachel@gmail.com>
2018-01-23 23:08:44 +0100

    Add some refactor to code

--- src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
@@ -48,15 +48,33 @@ public class CodecoolerView extends UserView{
     }
 
     public void showBuyTeamArtifactMenu(ArrayList<String> namesOfArtifacts, ArrayList<Long> collected, ArrayList<Long> prices, long balance){
-        System.out.println(CLEAR_CONSOLE);
-        System.out.print(Colors.GREEN_BOLD_BRIGHT + "MAGIC ARTIFACTS SHOP" + Colors.BLACK_BOLD_BRIGHT);
-        System.out.println("         YOUR MONEY: " + Colors.YELLOW_BOLD_BRIGHT + balance + "cc\n" + Colors.RESET);
+        System.out.println(String.format(
+                "%s%sMAGIC ARTIFACTS SHOP         %sYOUR MONEY: %s%scc\n%s",
+                CLEAR_CONSOLE, Colors.GREEN_BOLD_BRIGHT, Colors.BLACK_BOLD_BRIGHT, Colors.YELLOW_BOLD_BRIGHT,
+                balance, Colors.RESET));
         for (String artifact : namesOfArtifacts) {
             int index = namesOfArtifacts.indexOf(artifact);
-            System.out.print((index + 1) + ". " + Colors.CYAN_BOLD_BRIGHT + artifact);
-            System.out.println("   " + Colors.YELLOW_BOLD_BRIGHT + collected.get(index) + Colors.RESET + "/" + Colors.YELLOW_BOLD_BRIGHT + prices.get(index) + "cc" + Colors.RESET);
+            System.out.println(String.format(
+                        "%s. %s%s   %s%scc%s/%s%scc%s",
+                    index, Colors.WHITE_BOLD, artifact, Colors.YELLOW_BOLD_BRIGHT, prices.get(index), Colors.RESET,
+                    Colors.YELLOW_BOLD_BRIGHT, collected.get(index), Colors.RESET));
         }
         System.out.println("\n\n0 - Back");
+    }
+
+    public void showQuests(ArrayList<String> quests){
+        System.out.println(CLEAR_CONSOLE + Colors.GREEN_BOLD_BRIGHT + "\nTAVERN\n" + Colors.RESET);
+        String[] questInfo;
+        for (String quest : quests){
+            questInfo = quest.split("@");
+            String title = questInfo[0], reward = questInfo[1], description = questInfo[2];
+            System.out.println(String.format(
+                    "%s%s     %sReward: %s%scc\n%s%s\n",
+                    Colors.WHITE_BOLD, title, Colors.BLACK_BOLD_BRIGHT, Colors.YELLOW_BOLD_BRIGHT, reward,
+                    Colors.RESET, description));
+        }
+        System.out.println("\nPress enter to continue...");
+        getUserInput();
     }
 
     public void notEnoughCoolcoins(){
@@ -76,20 +94,5 @@ public class CodecoolerView extends UserView{
 
     public void showWrongInput() {
         System.out.println("Your input is wrong.\n");
-    }
-
-    public void showQuests(ArrayList<String> quests){
-        System.out.println(CLEAR_CONSOLE + Colors.GREEN_BOLD_BRIGHT + "\nTAVERN\n" + Colors.RESET);
-        String[] questInfo = new String[3];
-        for (String quest : quests){
-            questInfo = quest.split("@");
-            String title = questInfo[0];
-            String reward = questInfo[1];
-            String description = questInfo[2];
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + title + Colors.RESET + "     Reward: " + Colors.YELLOW_BOLD_BRIGHT + reward + " coolcoins");
-            System.out.println(Colors.RESET + description + "\n");
-        }
-        System.out.println("\nPress enter to continue...");
-        getUserInput();
     }
 }
