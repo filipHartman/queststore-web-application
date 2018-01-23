1483b15a949168b69ce2e218ab0b4c7a03fae2d9
siemaeniu500 <siemaeniu500@gmail.com>
2018-01-23 12:32:06 +0100

    Add display method for creating passwords process.

--- src/main/java/com/codecool/idontspeakjava/queststore/views/LoginView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/LoginView.java
@@ -1,6 +1,10 @@
 package com.codecool.idontspeakjava.queststore.views;
 
+import java.io.Console;
+
 public class LoginView extends UserView {
+
+    private static final Console console = System.console();
 
     public void showGreeting() {
         System.out.println(String.format("%sWelcome to %sQuest Store 1.0-SNAPSHOT", Colors.GREEN, Colors.YELLOW));
@@ -15,5 +19,23 @@ public class LoginView extends UserView {
     public String getUserLogin() {
         System.out.print(String.format("%sYour email: %s", Colors.BLUE, Colors.RESET));
         return getUserInput();
+    }
+
+    public String getUserPassword() {
+        return String.valueOf(console.readPassword(String.format("%sYour password: %s", Colors.BLUE, Colors.RESET)));
+    }
+
+    public String getNewUserPassword() {
+        System.out.println(String.format("%sPassword requirments:%s \n" +
+                        "%s-%s Min. 8-char length\n" +
+                        "%s-%s At least one of the following: diggit, upper case, lower case, special character"
+                , Colors.GREEN, Colors.RESET, Colors.BLUE, Colors.PURPLE, Colors.BLUE, Colors.PURPLE));
+        return String.valueOf(console.readPassword(String.valueOf("%sEnter new password:%s "), Colors.GREEN, Colors.RESET));
+    }
+
+    public void showNewPasswordIsIncorrect() {
+        clearScreen();
+        System.out.println(String.format("%sYour new password does not match requirements !%s", Colors.RED, Colors.RESET));
+        getUserInput();
     }
 }
