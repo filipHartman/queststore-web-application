be31efe835467d075d0d0aa152e0874518ab3337
przemekNachel <przemeknachel@gmail.com>
2018-01-23 11:04:31 +0100

    Add colours to codecooler menu

--- src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/views/CodecoolerView.java
@@ -20,27 +20,27 @@ public class CodecoolerView extends UserView{
     public void showWallet(long totalMoney, ArrayList<String> namesOfArtifacts){
         System.out.println(CLEAR_CONSOLE);
         System.out.println(Colors.GREEN_BOLD_BRIGHT + "WALLET\n" + Colors.RESET);
-        System.out.println("Your coolcoins: " + Colors.YELLOW_BOLD_BRIGHT + totalMoney + Colors.RESET);
-        System.out.println("\nYour artifacts: ");
+        System.out.println(Colors.BLACK_BOLD_BRIGHT + "Your coolcoins: " + Colors.YELLOW_BOLD_BRIGHT + totalMoney + Colors.BLACK_BOLD_BRIGHT);
+        System.out.println("\nYour artifacts: " + Colors.CYAN_BOLD_BRIGHT);
         for (String artifact : namesOfArtifacts) {
             System.out.println("- " + artifact);
         }
-        System.out.println("\n\nPress enter to continue...");
+        System.out.println(Colors.RESET + "\n\nPress enter to continue...");
         getUserInput();
     }
 
     public void showBuyArtifactMenu(ArrayList<String> namesOfArtifacts, ArrayList<Long> prices, long balance, ArtifactCategory category){
         System.out.println(CLEAR_CONSOLE);
         if (category == ArtifactCategory.Basic){
-            System.out.print("BASIC ARTIFACTS SHOP");
+            System.out.print(Colors.GREEN_BOLD_BRIGHT + "BASIC ARTIFACTS SHOP" + Colors.BLACK_BOLD_BRIGHT);
         } else {
-            System.out.print("MAGIC ARTIFACTS SHOP");
+            System.out.print(Colors.GREEN_BOLD_BRIGHT + "MAGIC ARTIFACTS SHOP" + Colors.BLACK_BOLD_BRIGHT);
         }
-        System.out.println("         YOUR MONEY: " + balance + "cc\n");
+        System.out.println("         YOUR MONEY: " + Colors.YELLOW_BOLD_BRIGHT + balance + "cc\n" + Colors.RESET);
         for (String artifact : namesOfArtifacts) {
             int index = namesOfArtifacts.indexOf(artifact);
-            System.out.print((index + 1) + ". " + artifact);
-            System.out.println("   " + prices.get(index) + "cc");
+            System.out.print((index + 1) + ". " + Colors.CYAN_BOLD_BRIGHT + artifact);
+            System.out.println("   " + Colors.YELLOW_BOLD_BRIGHT + prices.get(index) + "cc" + Colors.RESET);
         }
         System.out.println("\n\n0 - Back");
     }
@@ -56,15 +56,15 @@ public class CodecoolerView extends UserView{
     }
 
     public void showQuests(ArrayList<String> quests){
-        System.out.println(CLEAR_CONSOLE);
+        System.out.println(CLEAR_CONSOLE + Colors.GREEN_BOLD_BRIGHT + "\nTAVERN\n" + Colors.RESET);
         String[] questInfo = new String[3];
         for (String quest : quests){
             questInfo = quest.split("@");
             String title = questInfo[0];
             String reward = questInfo[1];
             String description = questInfo[2];
-            System.out.println(title + "     Reward: " + reward + " coolcoins");
-            System.out.println(description + "\n");
+            System.out.println(Colors.CYAN_BOLD_BRIGHT + title + Colors.RESET + "     Reward: " + Colors.YELLOW_BOLD_BRIGHT + reward + " coolcoins");
+            System.out.println(Colors.RESET + description + "\n");
         }
         System.out.println("\nPress enter to continue...");
         getUserInput();
