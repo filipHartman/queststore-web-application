ee25bea908b856021f434afbc3840e4fe6c4c7ac
Jacek_R <jacek.ragann@gmail.com>
2018-01-25 11:24:44 +0100

    Add inheritance from the Editor and delegate most of the code to it

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/ArtifactEditor.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/ArtifactEditor.java
@@ -2,187 +2,78 @@ package com.codecool.idontspeakjava.queststore.controllers.mentor;
 
 import com.codecool.idontspeakjava.queststore.database.ArtifactsDAO;
 import com.codecool.idontspeakjava.queststore.models.Artifact;
-import com.codecool.idontspeakjava.queststore.models.ArtifactCategory;
 import com.codecool.idontspeakjava.queststore.views.MentorView;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-class ArtifactEditor {
+class ArtifactEditor extends Editor {
 
     private static final String EDIT_TITLE = "1";
     private static final String EDIT_DESCRIPTION = "2";
     private static final String EDIT_CATEGORY = "3";
     private static final String EDIT_PRICE = "4";
 
-    private static final String EXIT = "0";
-
-    private MentorView view;
     private List<Artifact> artifacts;
-    private Artifact selectedArtifact;
 
     ArtifactEditor(MentorView view) {
-        this.view = view;
+        super(view, "Artifact");
         artifacts = new ArtifactsDAO().getAllArtifacts();
     }
 
-    void editArtifact() {
-        boolean continueLoop = true;
-        if (artifacts.isEmpty()) {
-            view.showNoArtifacts();
-            return;
-        }
-        while (continueLoop) {
-            view.selectArtifacts(getArtifactsTitles());
-
-            String userInput = view.getUserInput();
-
-            if (isArtifactSelected(userInput)) {
-                view.selectAttributeOfArtifactToEdit();
-                userInput = view.getUserInput();
-                continueLoop = checkUserInput(userInput);
-            } else {
-                if (userInput.equals(EXIT)) {
-                    continueLoop = false;
-                    view.showOperationCancelled();
-                } else {
-                    view.showWrongInput();
-                }
-            }
-        }
-    }
-
-    private boolean isArtifactSelected(String input) {
-        boolean inputIsValid = false;
-        if (input.matches("\\d+")) {
-            int inputAsInt = Integer.parseInt(input);
-            if (inputAsInt > 0 && inputAsInt <= artifacts.size()) {
-                selectedArtifact = artifacts.get(inputAsInt - 1);
-                inputIsValid = true;
-            }
-        }
-        return inputIsValid;
-    }
-
-    private ArrayList<String> getArtifactsTitles() {
-        ArrayList<String> titles = new ArrayList<>();
-        for (Artifact artifact : artifacts) {
-            titles.add(artifact.getTitle());
-        }
-        return titles;
-    }
-
-    private boolean checkUserInput(String input) {
-        boolean continueProgram = true;
+    @Override
+    void selectPrompt(String input) {
+        Artifact artifact = artifacts.get(indexOfItemToEdit);
         switch (input) {
             case EDIT_TITLE:
+                view.showTitle(artifact.getTitle());
                 view.askForArtifactTitle();
-                editTitle(selectedArtifact);
-                break;
-            case EDIT_DESCRIPTION:
-                view.askForArtifactDescription();
-                editDescription(selectedArtifact);
                 break;
             case EDIT_CATEGORY:
                 view.askForArtifactCategory();
-                editCategory(selectedArtifact);
+                view.showCategory(artifact.getCategory().toString());
+                break;
+            case EDIT_DESCRIPTION:
+                view.showDescription(artifact.getDescription());
+                view.askForArtifactDescription();
                 break;
             case EDIT_PRICE:
+                view.showPrice(String.valueOf(artifact.getPrice()));
                 view.askForArtifactPrice();
-                editPrice(selectedArtifact);
-                break;
-            case EXIT:
-                view.showOperationCancelled();
-                continueProgram = false;
-                break;
-            default:
-                view.showWrongInput();
-        }
-        return continueProgram;
-    }
-
-    private void editPrice(Artifact artifact) {
-        view.showPrice(String.valueOf(artifact.getPrice()));
-        String newPrice = view.getUserInput();
-        if (newPrice.equals(EXIT)) {
-            view.showOperationCancelled();
-            return;
-        }
-        if (newPrice.matches("\\d+")) {
-            int inputAsInt = Integer.valueOf(newPrice);
-            if (inputAsInt > 0) {
-                artifact.setPrice(inputAsInt);
-                new ArtifactsDAO().updateArtifact(artifact);
-            } else {
-                view.showInputMustBeHigherThanZero();
-            }
-        } else {
-            view.showWrongDigitInput();
-        }
-    }
-
-    private void editDescription(Artifact artifact) {
-        view.showDescription(artifact.getDescription());
-        String newDescription = view.getUserInput();
-        if (newDescription.equals(EXIT)) {
-            view.showOperationCancelled();
-            return;
-        }
-        if (newDescription.matches("[a-zA-Z1-9,.! ]+")) {
-            artifact.setDescription(newDescription);
-            new ArtifactsDAO().updateArtifact(artifact);
-        } else {
-            view.showWrongDescriptionInput();
-        }
-    }
-
-    private void editCategory(Artifact artifact) {
-        final String BASIC = "1";
-        final String MAGIC = "2";
-        view.showCategory(artifact.getCategory().toString());
-        String newCategory = view.getUserInput();
-        if (newCategory.equals(EXIT)) {
-            view.showOperationCancelled();
-            return;
-        }
-        ArtifactsDAO artifactsDAO = new ArtifactsDAO();
-        switch (newCategory) {
-            case BASIC:
-                artifact.setCategory(ArtifactCategory.Basic);
-                artifactsDAO.updateArtifact(artifact);
-                break;
-            case MAGIC:
-                artifact.setCategory(ArtifactCategory.Magic);
-                artifactsDAO.updateArtifact(artifact);
                 break;
             default:
                 view.showWrongInput();
         }
     }
 
-    private void editTitle(Artifact artifact) {
-        view.showTitle(artifact.getTitle());
-        String newTitle = view.getUserInput();
-        if (newTitle.equals(EXIT)) {
-            view.showOperationCancelled();
-            return;
+    @Override
+    void editAttribute(String selectedOption, String input) {
+        Artifact artifact = artifacts.get(indexOfItemToEdit);
+        List<String> titles = getTitles();
+
+        switch (selectedOption) {
+            case EDIT_TITLE:
+                editTitle(artifact, titles, input);
+                break;
+            case EDIT_CATEGORY:
+                editCategory(artifact, input);
+                break;
+            case EDIT_DESCRIPTION:
+                editDescription(artifact, input);
+                break;
+            case EDIT_PRICE:
+                editPrice(artifact, input);
+                break;
         }
-        if (newTitle.matches("[a-zA-Z1-9 ]+")) {
-            try {
-                ArtifactsDAO artifactsDAO = new ArtifactsDAO();
-                if (!artifactsDAO.checkIfArtifactExists(newTitle)) {
-                    artifact.setTitle(newTitle);
-                    artifactsDAO.updateArtifact(artifact);
-                } else {
-                    view.showDuplicateWarning();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                view.showDatabaseError();
-            }
-        } else {
-            view.showWrongTitleInput();
-        }
+    }
+
+    @Override
+    public List getCollection() {
+        return artifacts;
+    }
+
+    @Override
+    public List<String> getTitles() {
+        return artifacts.stream().map(Artifact::getTitle).collect(Collectors.toList());
     }
 }
