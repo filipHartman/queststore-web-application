8dcf51ddebce24ff8213cad651012442d090cab5
Jacek_R <jacek.ragann@gmail.com>
2018-02-06 12:04:35 +0100

    Create method for validating an email

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/Validator.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/Validator.java
@@ -8,6 +8,12 @@ public class Validator {
     public final static String BASIC_CATEGORY = "1";
     private final static String EXTRA_CATEGORY = "2";
 
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:" +
+            "[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(" +
+            "?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]" +
+            "?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[" +
+            "\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
     public boolean checkIfIsPositiveInteger(String input) {
         boolean priceIsValid = false;
         if (input.matches("\\d+")) {
@@ -57,5 +63,15 @@ public class Validator {
             nameIsValid = true;
         }
         return nameIsValid;
+    }
+
+    public boolean checkIfEmailIsValid(String input, List<String> emails) {
+        boolean emailIsValid = false;
+        if (input.matches(EMAIL_REGEX)) {
+            if (!emails.contains(input)) {
+                emailIsValid = true;
+            }
+        }
+        return emailIsValid;
     }
 }
