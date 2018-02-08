1558dbc455a6a6f03c302739b6cfeeeae7cf37a9
Jacek_R <jacek.ragann@gmail.com>
2018-02-08 10:26:49 +0100

    Create implementation of tests for checkIfTitleIsValid method

--- src/test/java/com/codecool/idontspeakjava/queststore/mentor/ValidatorTest.java
+++ src/test/java/com/codecool/idontspeakjava/queststore/mentor/ValidatorTest.java
@@ -3,6 +3,9 @@ package com.codecool.idontspeakjava.queststore.mentor;
 import com.codecool.idontspeakjava.queststore.controllers.mentor.Validator;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -34,12 +37,30 @@ public class ValidatorTest {
     }
 
     @Test
-    public void checkIfDescriptionIsValidShouldReturnFalseWhenHaveCharacterOtherThanDotSpaceOrComma() {
+    public void checkIfDescriptionIsValidShouldReturnFalseWhenHaveCharacterOtherThanDotSpaceDigitsLettersOrComma() {
         assertFalse(new Validator().checkIfDescriptionIsValid("Ultra super, test % .."));
     }
 
     @Test
     public void checkIfDescriptionIsValidShouldReturnTrueWhenDescriptionDontHaveOtherCharsThanDotSpaceOrComma() {
         assertTrue(new Validator().checkIfDescriptionIsValid("This is, a, correct description. Probably."));
+    }
+
+    @Test
+    public void checkIfTitleIsValidShouldReturnFalseWhenHaveCharsOtherThanLetters() {
+        List<String> titles = Arrays.asList("Title", "Title 2", "Title 3");
+        assertFalse(new Validator().checkIfTitleIsValid("Title 4 !", titles));
+    }
+
+    @Test
+    public void checkIfTitleIsValidShouldReturnFalseWhenTitleExists() {
+        List<String> titles = Arrays.asList("Title", "Title 2", "Title 3");
+        assertFalse(new Validator().checkIfTitleIsValid("Title 2", titles));
+    }
+
+    @Test
+    public void checkIfTitleIsValidShouldReturnTrueWhenHaveLettersNumbersSpacesAndIsUnique() {
+        List<String> titles = Arrays.asList("Title", "Title 2", "Title 3");
+        assertTrue(new Validator().checkIfTitleIsValid("Title 4", titles));
     }
 }
