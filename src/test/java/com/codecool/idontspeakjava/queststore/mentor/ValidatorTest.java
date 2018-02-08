35e969ef362ad958798bbfcfa95004154dc62963
Jacek_R <jacek.ragann@gmail.com>
2018-02-08 09:50:55 +0100

    Create empty methods for testing checkIfPositiveInteger method

--- src/test/java/com/codecool/idontspeakjava/queststore/mentor/ValidatorTest.java
+++ src/test/java/com/codecool/idontspeakjava/queststore/mentor/ValidatorTest.java
@@ -4,4 +4,23 @@ import org.junit.Test;
 
 public class ValidatorTest {
 
+    @Test
+    public void checkIfPositiveIntegerShouldReturnFalseWhenInputIsText() {
+    }
+
+    @Test
+    public void checkIfPositiveIntegerShouldReturnFalseWhenInputIsFloat() {
+    }
+
+    @Test
+    public void checkIfPositiveIntegerShouldReturnFalseWhenInputIsZero() {
+    }
+
+    @Test
+    public void checkIfPositiveIntegerShouldReturnFalseWhenInputIsBelowZero() {
+    }
+
+    @Test
+    public void checkIfPositiveIntegerShouldReturnTrueWhenInputIsAnIntegerAboveZero() {
+    }
 }
