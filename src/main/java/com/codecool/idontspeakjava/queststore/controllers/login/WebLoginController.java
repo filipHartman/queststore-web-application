6aa4a04928966cc9d6b5c45d938070738c89ba11
Joanna Baran <joannabaran56@gmail.com>
2018-04-16 16:00:48 +0200

    Add displaying login form

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/login/WebLoginController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/login/WebLoginController.java
@@ -1,4 +1,29 @@
 package com.codecool.idontspeakjava.queststore.controllers.login;
 
-public class WebLoginController {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WebLoginController implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/login.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        String response = template.render(model);
+
+        byte[] bytes = response.getBytes();
+        try {
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
