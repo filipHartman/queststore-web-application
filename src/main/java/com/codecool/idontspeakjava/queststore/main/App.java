2c43c934acc0eda7ce09ffb601877df60bb8888f
Joanna Baran <joannabaran56@gmail.com>
2018-04-16 14:48:38 +0200

    Add http server creation

--- src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/main/App.java
@@ -1,12 +1,25 @@
 package com.codecool.idontspeakjava.queststore.main;
 
+import com.codecool.idontspeakjava.queststore.controllers.login.WebLoginController;
+import com.sun.net.httpserver.HttpServer;
 
-import com.codecool.idontspeakjava.queststore.controllers.root.terminal.LoginController;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class App {
 
     public static void main(String[] args) {
-        new LoginController();
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(8000),0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.createContext("/login", new WebLoginController());
+        server.setExecutor(null);
+
+        server.start();
     }
 
 }
