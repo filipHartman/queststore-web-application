8f47af915aa5c1f9755142b301a341c3f3d6aae5
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-04-18 15:36:26 +0200

    Remove unused imports

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/admin/web/WebAdminController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/admin/web/WebAdminController.java
@@ -3,8 +3,6 @@ package com.codecool.idontspeakjava.queststore.controllers.admin.web;
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
 import com.sun.net.httpserver.HttpExchange;
 
-import java.io.IOException;
-
 public class WebAdminController extends AbstractHandler {
 
     @Override
--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebMentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebMentorController.java
@@ -1,11 +1,7 @@
 package com.codecool.idontspeakjava.queststore.controllers.mentor.web;
 
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
-import com.codecool.idontspeakjava.queststore.controllers.admin.web.WebAssignMentor;
-import com.codecool.idontspeakjava.queststore.controllers.admin.web.WebCreateMentor;
 import com.sun.net.httpserver.HttpExchange;
-
-import java.io.IOException;
 
 public class WebMentorController extends AbstractHandler {
 
