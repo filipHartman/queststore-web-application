12350c0108e082e2cf15cf4fe8fff20e84d6279f
filipHartman <filip.hartman23@gmail.com>
2018-04-18 15:08:17 +0200

    Add handler to create new class

---
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/admin/web/WebCreateClass.java
@@ -0,0 +1,32 @@
+package com.codecool.idontspeakjava.queststore.controllers.admin.web;
+
+import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.codecool.idontspeakjava.queststore.controllers.helpers.HTMLGenerator;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteCodecoolClassDAO;
+import com.codecool.idontspeakjava.queststore.models.CodecoolClass;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class WebCreateClass extends AbstractHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+
+        String form = HTMLGenerator.generateFromToCreateClass("Create codecool class");
+
+        if(method.equals("GET")) {
+            sendTemplateResponseWithForm(httpExchange, "admin_home", form);
+        }
+
+        if(method.equals("POST")) {
+            Map<String, String> data = readFormData(httpExchange);
+
+            String name = data.get("name");
+            new SQLiteCodecoolClassDAO().createCodecoolClass(new CodecoolClass(name));
+
+            redirectToLocation(httpExchange, "/");
+        }
+    }
+}
