eaf3eba5ce1dfe3e6ae1d25a2ef64946ce2d33ac
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-04-18 16:29:54 +0200

    Create class WebCreateLevel

---
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/admin/web/WebCreateLevel.java
@@ -0,0 +1,13 @@
+package com.codecool.idontspeakjava.queststore.controllers.admin.web;
+
+import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class WebCreateLevel extends AbstractHandler {
+    @Override
+    public void handle(HttpExchange httpExchange){
+
+    }
+}
