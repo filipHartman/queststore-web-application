87f8e2396117f5faf5331eab3f6f1b0ce285bf82
martastaporek <staporekmarta1@gmail.com>
2018-04-19 16:04:12 +0200

    Implement show Wallet

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebShowWallet.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebShowWallet.java
@@ -3,11 +3,16 @@ package com.codecool.idontspeakjava.queststore.controllers.mentor.web;
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
 import com.codecool.idontspeakjava.queststore.controllers.helpers.HTMLGenerator;
 import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteUserDAO;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteWalletsDAO;
 import com.codecool.idontspeakjava.queststore.models.Permissions;
 import com.codecool.idontspeakjava.queststore.models.User;
+import com.codecool.idontspeakjava.queststore.models.Wallet;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 
 public class WebShowWallet extends AbstractHandler {
@@ -16,14 +21,27 @@ public class WebShowWallet extends AbstractHandler {
     @Override
     public void handle(HttpExchange httpExchange) {
 
+        List<User> allStudents = new SQLiteUserDAO().getUsersByPermission(Permissions.Student);
+
         String method = httpExchange.getRequestMethod();
          if(method.equals("GET")){
-             List<User> allStudents = new SQLiteUserDAO().getUsersByPermission(Permissions.Student);
+
              String form = HTMLGenerator.getRadioForm(allStudents, "Choose student", "name");
              sendTemplateResponseWithForm(httpExchange, "mentor_home", form);
          }
 
          if(method.equals("POST")){
+
+             Map<String, String> data = readFormData(httpExchange);
+             String userName = data.get("name");
+             User choosenUser = getChosenUser(allStudents, userName);
+             int choosenUserId = choosenUser.getId();
+             Wallet choosenUserWallet = new SQLiteWalletsDAO().getWalletByUserID(choosenUserId);
+             List <String> walletData = asList( "Total earing: " + choosenUserWallet.getTotalEarnings(),
+                     "Current State:"+ choosenUserWallet.getCurrentState());
+             String responseForm = HTMLGenerator.getList(walletData, choosenUser.getFullName());
+             sendTemplateResponseWithForm(httpExchange, "mentor_home", responseForm);
+
 
          }
 
