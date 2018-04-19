be9020c340909e9cd666da65b40713f6b01c5c37
Joanna Baran <joannabaran56@gmail.com>
2018-04-19 11:27:24 +0200

    Add generating templateName from home location

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/AlertController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/AlertController.java
@@ -4,23 +4,42 @@ import com.codecool.idontspeakjava.queststore.controllers.helpers.HTMLGenerator;
 import com.sun.net.httpserver.HttpExchange;
 
 public class AlertController extends AbstractHandler {
+
     @Override
     public void handle(HttpExchange exchange) {
-        String action = getAction(exchange);
-        String method = exchange.getRequestMethod();
-
-        if (method.equals("GET") && action.equals("success")) {
-            String message = "Operation successful!";
-            String form = HTMLGenerator.getAlertForm(message);
-            sendTemplateResponseWithForm(exchange, "admin_home", form);
-
-        } else if (method.equals("GET") && action.equals("fail")) {
-            String message = "Operation failed!";
-            String form = HTMLGenerator.getAlertForm(message);
-            sendTemplateResponseWithForm(exchange, "admin_home", form);
-
-        } else if (method.equals("POST")) {
+        String cookieStr = exchange.getRequestHeaders().getFirst("Cookie");
+        String sid;
+        if (cookieStr == null) {
             redirectToLocation(exchange, "/");
+            return;
+        } else {
+            sid = getSidFromCookieStr(cookieStr);
         }
+
+        if (!isLoggedIn(sid)) {
+            redirectToLocation(exchange, "/");
+        } else {
+            String action = getAction(exchange);
+            String method = exchange.getRequestMethod();
+            String templateName = getTemplateNameFromHomeLocation(getHomeLocationFromSid(sid));
+
+            if (method.equals("GET") && action.equals("success")) {
+                String message = "Operation successful!";
+                String form = HTMLGenerator.getAlertForm(message);
+                sendTemplateResponseWithForm(exchange, templateName, form);
+
+            } else if (method.equals("GET") && action.equals("fail")) {
+                String message = "Operation failed!";
+                String form = HTMLGenerator.getAlertForm(message);
+                sendTemplateResponseWithForm(exchange, templateName, form);
+
+            } else if (method.equals("POST")) {
+                redirectToLocation(exchange, "/");
+            }
+        }
+    }
+
+    private String getTemplateNameFromHomeLocation(String homeLocation) {
+        return String.format("%s_home", homeLocation.substring(1));
     }
 }
