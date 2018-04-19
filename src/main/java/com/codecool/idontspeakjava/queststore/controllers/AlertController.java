f53fec5d765f65b5622a0ca5bd0631c718939039
Joanna Baran <joannabaran56@gmail.com>
2018-04-19 11:46:15 +0200

    Refactor handle method in AlertController

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/AlertController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/AlertController.java
@@ -8,34 +8,34 @@ public class AlertController extends AbstractHandler {
     @Override
     public void handle(HttpExchange exchange) {
         String cookieStr = exchange.getRequestHeaders().getFirst("Cookie");
-        String sid;
         if (cookieStr == null) {
             redirectToLocation(exchange, "/");
             return;
-        } else {
-            sid = getSidFromCookieStr(cookieStr);
         }
+
+        String sid = getSidFromCookieStr(cookieStr);
 
         if (!isLoggedIn(sid)) {
             redirectToLocation(exchange, "/");
-        } else {
-            String action = getAction(exchange);
-            String method = exchange.getRequestMethod();
-            String templateName = getTemplateNameFromHomeLocation(getHomeLocationFromSid(sid));
+            return;
+        }
 
-            if (method.equals("GET") && action.equals("success")) {
-                String message = "Operation successful!";
-                String form = HTMLGenerator.getAlertForm(message);
-                sendTemplateResponseWithForm(exchange, templateName, form);
+        String action = getAction(exchange);
+        String method = exchange.getRequestMethod();
+        String templateName = getTemplateNameFromHomeLocation(getHomeLocationFromSid(sid));
 
-            } else if (method.equals("GET") && action.equals("fail")) {
-                String message = "Operation failed!";
-                String form = HTMLGenerator.getAlertForm(message);
-                sendTemplateResponseWithForm(exchange, templateName, form);
+        if (method.equals("GET") && action.equals("success")) {
+            String message = "Operation successful!";
+            String form = HTMLGenerator.getAlertForm(message);
+            sendTemplateResponseWithForm(exchange, templateName, form);
 
-            } else if (method.equals("POST")) {
-                redirectToLocation(exchange, "/");
-            }
+        } else if (method.equals("GET") && action.equals("fail")) {
+            String message = "Operation failed!";
+            String form = HTMLGenerator.getAlertForm(message);
+            sendTemplateResponseWithForm(exchange, templateName, form);
+
+        } else if (method.equals("POST")) {
+            redirectToLocation(exchange, "/");
         }
     }
 
