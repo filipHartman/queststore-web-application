84e60a43712da0804eddde7e0cb5c3b3aba18e0f
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-04-19 17:34:58 +0200

    Add artifacts info to wallet info

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebShowWallet.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebShowWallet.java
@@ -2,13 +2,15 @@ package com.codecool.idontspeakjava.queststore.controllers.mentor.web;
 
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
 import com.codecool.idontspeakjava.queststore.controllers.helpers.HTMLGenerator;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteArtifactsDAO;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteOrdersDAO;
 import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteUserDAO;
 import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteWalletsDAO;
-import com.codecool.idontspeakjava.queststore.models.Permissions;
-import com.codecool.idontspeakjava.queststore.models.User;
-import com.codecool.idontspeakjava.queststore.models.Wallet;
+import com.codecool.idontspeakjava.queststore.models.*;
 import com.sun.net.httpserver.HttpExchange;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -37,13 +39,41 @@ public class WebShowWallet extends AbstractHandler {
              User choosenUser = getChosenUser(allStudents, userName);
              int choosenUserId = choosenUser.getId();
              Wallet choosenUserWallet = new SQLiteWalletsDAO().getWalletByUserID(choosenUserId);
-             List <String> walletData = asList( "Total earing: " + choosenUserWallet.getTotalEarnings(),
-                     "Current State:"+ choosenUserWallet.getCurrentState());
+             List <String> walletData = new ArrayList<>(Arrays.asList(
+                     "Total earing: " + choosenUserWallet.getTotalEarnings(),
+                     "Current State:"+ choosenUserWallet.getCurrentState(),
+                     "\n",
+                     "Artifacts:"
+             ));
+
+
+             List<String> test = getUserOrders(choosenUser);
+             walletData.addAll(test);
+
              String responseForm = HTMLGenerator.getList(walletData, choosenUser.getFullName());
              sendTemplateResponseWithForm(httpExchange, "mentor_home", responseForm);
-
 
          }
 
     }
+
+    private List<String> getUserOrders(User user){
+        List<Order> orders = new SQLiteOrdersDAO().getAllOrdersByUser(user);
+        return createInfoAboutOrders(orders);
+
+    }
+
+    private List<String> createInfoAboutOrders(List<Order> orders) {
+        SQLiteArtifactsDAO dao = new SQLiteArtifactsDAO();
+        List<String> ordersToPrint = new ArrayList<>();
+
+        for (Order o : orders) {
+            String orderInfo = dao.getArtifact(o.getArtifactID()).getTitle() + ", ";
+            orderInfo += o.isUsed() ? "Used" : "Not used" + ", ";
+            orderInfo += o instanceof TeamOrder ? "Magic" : "Basic";
+            ordersToPrint.add(orderInfo);
+        }
+        return ordersToPrint;
+    }
+
 }
