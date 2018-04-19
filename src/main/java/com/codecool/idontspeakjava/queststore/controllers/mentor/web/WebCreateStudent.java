5e625b7af1b73327d0d54b33be171fb927ebc164
martastaporek <staporekmarta1@gmail.com>
2018-04-19 13:40:19 +0200

    Implement create studnet

---
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebCreateStudent.java
@@ -0,0 +1,50 @@
+package com.codecool.idontspeakjava.queststore.controllers.mentor.web;
+
+import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.codecool.idontspeakjava.queststore.controllers.helpers.HTMLGenerator;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteUserDAO;
+import com.codecool.idontspeakjava.queststore.models.Permissions;
+import com.codecool.idontspeakjava.queststore.models.User;
+import com.codecool.idontspeakjava.queststore.services.PasswordService;
+import com.sun.net.httpserver.HttpExchange;
+
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public class WebCreateStudent extends AbstractHandler{
+
+    public void handle(HttpExchange httpExchange) {
+
+        String method = httpExchange.getRequestMethod();
+
+        if (method.equals("GET")) {
+            System.out.println(HTMLGenerator.generateFormToCreateUser("dupa"));
+            sendTemplateResponseWithForm(httpExchange, "mentor_home", HTMLGenerator.generateFormToCreateUser("Create student"));
+
+        }
+
+        if (method.equals("POST")) {
+            Map<String, String> data = null;
+
+            data = readFormData(httpExchange);
+            String name = data.get("name");
+            String lastname = data.get("lastname");
+            String email = data.get("email");
+            String password = new PasswordService().hashPassword(data.get("password"));
+            User user = new User(name, lastname, password, email, Permissions.Student);
+            SQLiteUserDAO dao = new SQLiteUserDAO();
+            try {
+                dao.createUser(user);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                redirectToLocation(httpExchange, "/alert/fail");
+            }
+
+            redirectToLocation(httpExchange, "/alert/success");
+        }
+    }
+
+
+}
+
