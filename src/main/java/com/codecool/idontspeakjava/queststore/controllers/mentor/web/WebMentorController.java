72a57d76e7805f4d7ca7bafe032d85cd0e49a72b
martastaporek <staporekmarta1@gmail.com>
2018-04-19 15:26:56 +0200

    Implement choose student

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebMentorController.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/mentor/web/WebMentorController.java
@@ -43,6 +43,7 @@ public class WebMentorController extends AbstractHandler {
             case "choose-student-to-mark-artifact":
                 break;
             case "check-wallet":
+                new WebShowWallet().handle(httpExchange);
                 break;
             default:
                 sendTemplateResponse(httpExchange, "mentor_home");
