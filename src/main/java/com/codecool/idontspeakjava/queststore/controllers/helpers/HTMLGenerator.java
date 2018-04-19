4dc7dcb24f5022552c74189c8597c417f31f2f81
Joanna Baran <joannabaran56@gmail.com>
2018-04-19 18:34:20 +0200

    Add generating form to edit quest

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/helpers/HTMLGenerator.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/helpers/HTMLGenerator.java
@@ -212,4 +212,23 @@ public class HTMLGenerator {
                 ).with(getLegend("Add artifact"))
         ).render();
     }
+
+    public static String generateFormToEditQuest(List<?> collection) {
+        return div(
+                fieldset(label().with(span("Choose quest")).withClass("subtitle"),
+                        form(each(collection, item ->
+                                label(item.toString())
+                                        .with(getRadioInput("quest", item.toString()))
+                        ),
+                                label().with(span("Edit quest")).withClass("subtitle"),
+                                label().with(span("Title")).with(getInput("title")),
+                                label().with(span("Description")).with(getInput("description")),
+                                label().with(span("Reward")).with(getInput("reward").withType("number")),
+                                label().with(span("Choose category")).withClass("subtitle"),
+                                label("Basic").with(getRadioInput("category", "Basic" )),
+                                label("Extra").with(getRadioInput("category", "Extra" ))
+                ).with(getButton("Submit"))
+                ).with(getLegend("Edit quest"))
+        ).withClass("form-style").render();
+    }
 }
