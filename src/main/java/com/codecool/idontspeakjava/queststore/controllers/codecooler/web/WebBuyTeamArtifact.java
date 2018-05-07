3c866165ece5e33ade1f128b66602cf968f146ba
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-05-07 14:58:22 +0200

    Create class WebBuyTeamArtifact and handler method

---
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/codecooler/web/WebBuyTeamArtifact.java
@@ -0,0 +1,11 @@
+package com.codecool.idontspeakjava.queststore.controllers.codecooler.web;
+
+import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class WebBuyTeamArtifact extends AbstractHandler{
+    @Override
+    public void handle(HttpExchange httpExchange){
+
+    }
+}
