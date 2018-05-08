d1d5416f86481aec1f452c71775d5e10730b4b1f
filipHartman <filip.hartman23@gmail.com>
2018-05-08 11:57:06 +0200

    Add manager handler for team options

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/codecooler/web/WebManageTeam.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/codecooler/web/WebManageTeam.java
@@ -1,6 +1,7 @@
 package com.codecool.idontspeakjava.queststore.controllers.codecooler.web;
 
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.codecool.idontspeakjava.queststore.models.Permissions;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
@@ -9,9 +10,31 @@ public class WebManageTeam extends AbstractHandler{
 
     @Override
     public void handle(HttpExchange httpExchange){
-        String method = httpExchange.getRequestMethod();
-        if(method.equals("GET")) {
-            sendTemplateResponse(httpExchange, "team_home");
+        if (checkPermission(httpExchange, Permissions.Student)) {
+            try {
+                redirectToActionHandler(httpExchange, getAction(httpExchange));
+            } catch (IndexOutOfBoundsException e) {
+                sendTemplateResponse(httpExchange, "team_home");
+            }
+        } else {
+            redirectToLocation(httpExchange, "/");
+        }
+    }
+
+    private void redirectToActionHandler(HttpExchange httpExchange, String action){
+        switch(action) {
+            case "create-team":
+                new WebCreateTeam().handle(httpExchange);
+                break;
+            case "join-team":
+//                new WebGetQuest().handle(httpExchange);
+                break;
+            case "leave-team":
+//                new WebSeeWallet().handle(httpExchange);
+                break;
+            default:
+                sendTemplateResponse(httpExchange, "team_home");
+                break;
         }
     }
 }
