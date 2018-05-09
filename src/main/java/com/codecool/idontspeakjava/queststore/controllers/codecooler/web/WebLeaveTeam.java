c27ff794bce7dcfbbd547afb582db12c8b73ccb6
filipHartman <filip.hartman23@gmail.com>
2018-05-09 11:40:16 +0200

    Add leaving team by student

--- src/main/java/com/codecool/idontspeakjava/queststore/controllers/codecooler/web/WebLeaveTeam.java
+++ src/main/java/com/codecool/idontspeakjava/queststore/controllers/codecooler/web/WebLeaveTeam.java
@@ -1,11 +1,19 @@
 package com.codecool.idontspeakjava.queststore.controllers.codecooler.web;
 
 import com.codecool.idontspeakjava.queststore.controllers.AbstractHandler;
+import com.codecool.idontspeakjava.queststore.database.sqlite.SQLiteTeamsDAO;
+import com.codecool.idontspeakjava.queststore.models.User;
 import com.sun.net.httpserver.HttpExchange;
 
 public class WebLeaveTeam extends AbstractHandler{
     @Override
     public void handle(HttpExchange httpExchange) {
+        User user = getUserBySession(httpExchange);
+        if (new SQLiteTeamsDAO().removeUserFromTeam(user)) {
+            redirectToLocation(httpExchange, "/alert/success");
+        } else {
+            redirectToLocation(httpExchange, "/alert/fail");
+        }
 
     }
 }
